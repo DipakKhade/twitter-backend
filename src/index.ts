90b8410ee5f7d220004f09291558849cbd656733
= <=>
2024-02-15 21:50:02 +0530

    initilizing docker containers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import express from 'express'
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 
-const app=express()
+(async()=>{
+    const app=express()
 const PORT=8000
 
+app.use(express.json())
+
+const graphqlserver = new ApolloServer({
+    typeDefs:`
+    type Query {
+        hello:String
+    }
+    `,
+    resolvers:{
+        Query:{
+            hello:()=>'this is a graphql'
+        }
+    },
+  });
+
+
+  await graphqlserver.start();
+  app.use('/graphql',expressMiddleware(graphqlserver))
 app.get('/',(req,res)=>{
     return res.send('server is running')
 })
@@ -10,3 +31,4 @@ app.get('/',(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
+})();
